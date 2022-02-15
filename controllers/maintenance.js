83b495a61ec6dea5f5981a256874901a8c884f02
Arvidvdc <arvidvdc@gmail.com>
2022-02-15 06:06:37 +0100

    Refactor maintenance-color create route.

--- controllers/maintenance.js
+++ controllers/maintenance.js
@@ -34,7 +34,6 @@ exports.colorAdd = (req,res) => {
         topline             = false,
         trendline           = false,
         ultraline           = false,
-
         al22                = false,
         al23                = false,
         al24                = false,
@@ -46,54 +45,22 @@ exports.colorAdd = (req,res) => {
         schuttingplanken    = false,
         screenlinef513zip   = false;
 
-    if(req.body.designline == "on") {
-        designline = true;
-    }
-    if(req.body.ecoline == "on") {
-        ecoline = true;
-    }
-    if(req.body.luxline == "on") {
-        luxline = true;
-    }
-    if(req.body.topline == "on") {
-        topline = true;
-    }
-    if(req.body.trendline == "on") {
-        trendline = true;
-    }
-    if(req.body.ultraline == "on") {
-        ultraline = true;
-    }
-    if(req.body.al22 == "on") {
-        al22 = true;
-    }
-    if(req.body.al23 == "on") {
-        al23 = true;
-    }
-    if(req.body.al24 == "on") {
-        al24 = true;
-    }
-    if(req.body.w350zip == "on") {
-        w350zip = true;
-    }
-    if(req.body.t350zip == "on") {
-        t350zip = true;
-    }
-    if(req.body.w350 == "on") {
-        w350 = true;
-    }
-    if(req.body.t350 == "on") {
-        t350 = true;
-    }
-    if(req.body.verandawanden == "on") {
-        verandawanden = true;
-    }
-    if(req.body.schuttingplanken == "on") {
-        schuttingplanken = true;
-    }
-    if(req.body.screenlinef513zip == "on") {
-        screenlinef513zip = true;
-    }
+    if(req.body.designline == "on") {designline = true;}
+    if(req.body.ecoline == "on") {ecoline = true;}
+    if(req.body.luxline == "on") {luxline = true;}
+    if(req.body.topline == "on") {topline = true;}
+    if(req.body.trendline == "on") {trendline = true;}
+    if(req.body.ultraline == "on") {ultraline = true;}
+    if(req.body.al22 == "on") {al22 = true;}
+    if(req.body.al23 == "on") {al23 = true;}
+    if(req.body.al24 == "on") {al24 = true;}
+    if(req.body.w350zip == "on") {w350zip = true;}
+    if(req.body.t350zip == "on") {t350zip = true;}
+    if(req.body.w350 == "on") {w350 = true;}
+    if(req.body.t350 == "on") {t350 = true;}
+    if(req.body.verandawanden == "on") {verandawanden = true;}
+    if(req.body.schuttingplanken == "on") {schuttingplanken = true;}
+    if(req.body.screenlinef513zip == "on") {screenlinef513zip = true;}
 
     let newColor = {kleurnummer: kleurnummer, kleuromschrijving: kleuromschrijving, designline: designline, ecoline: ecoline, luxline: luxline, topline: topline, trendline: trendline, ultraline: ultraline, al22: al22, al23: al23, al24: al24, w350zip: w350zip, t350zip: t350zip, w350: w350, t350: t350, verandawanden: verandawanden, schuttingplanken: schuttingplanken, screenlinef513zip: screenlinef513zip};
     COLOR.create(newColor, (err,color) => {
